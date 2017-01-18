6dd5c6694e197abe0fe52f591d6136d8e81ff8ea
Adar Butel <theadarbutel@gmail.com>
2017-01-17 19:04:56 -0800

    Calculator now works

--- js/script.js
+++ js/script.js
@@ -5,25 +5,116 @@ $(document).ready(function() {
     });
 });
 
-var total = 0;
-var operatorPressed = false;
+var prev = 0;
+var now = 0;
+var operatorPressed = undefined;
+var opEngaged = false;
 
-function keyPressed(num) {
-
-    var current = $("#screen").html()
-
-    if (num == "AC") {
+function keyPressed(key) {
+    var current = getScreen("str");
+    switch (key) {
+    case "AC":
+        opEngaged = false;
+        prev = 0;
+        now = 0;
+        operatorPressed = undefined;
         $("#screen").html("0");
-        return;
+        break;
+    case "+":
+    case "*":
+    case "/":
+    case "-":
+        handleOpPressed(key);
+        break;
+    case "=":
+        opEngaged = false;
+        handleEqualPressed(current);
+        break;
+    default:
+        current = (current == 0 || opEngaged) ? key : current + key;
+        $("#screen").html(current);
+        now = current;
+        opEngaged = false;
+        break;
+    }
+}
+
+var stringified = {
+    "0": "zero",
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine",
+    "+": "plus",
+    "-": "minus",
+    "*": "multiply",
+    "/": "divide"
+};
+
+// Handle Operator Pressed
+function handleOpPressed(op) {
+    var id = "#" + stringified[op];
+    var color = (operatorPressed) ? "red" : "blue";
+    $(id).css("background-color", color);
+    // If operator is already pressed, disable it.
+    if (operatorPressed) {
+        handleEqualPressed();
+        resetColors();
     }
 
-    if (num == "*") {
-        total = current;
-        $("#screen").html("0");
-        operatorPressed = !operatorPressed;
-        return;
+    operatorPressed = (operatorPressed) ? undefined : op;
+    opEngaged = (operatorPressed) ? true : false;
+
+    // Do math
+    prev = getScreen("num");
+}
+
+function handleEqualPressed() {
+    console.log("Solving");
+    var op = operatorPressed;
+    var id = "#" + stringified[op];
+    $(id).css("background-color", "red");
+    var results = 0;
+    now = getScreen("num");
+    prev = Number(prev);
+    switch (op) {
+    case "+":
+        results = prev + now;
+        break;
+    case "-":
+        results = prev - now;
+        break;
+    case "*":
+        results = prev * now;
+        break;
+    case "/":
+        results = prev / now;
+        break;
     }
 
-    current = (current == 0) ? num : current + num;
-    $("#screen").html(current);
+    console.log(op);
+    console.log(now);
+    console.log(prev);
+    prev = results;
+    $("#screen").html(results);
+    console.log(results);
+    operatorPressed = undefined;
+}
+
+function getScreen(type) {
+    return (type == "num") ? Number($("#screen").html()) : $("#screen").html();
+}
+
+function resetColors() {
+    var color = "red";
+    var prop = "background-color";
+    $("#" + stringified["+"]).css(prop, color);
+    $("#" + stringified["-"]).css(prop, color);
+    $("#" + stringified["*"]).css(prop, color);
+    $("#" + stringified["/"]).css(prop, color);
 }
